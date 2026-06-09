ed57a9db0e22042fc696d12a3c7e076f297d4c02
Divyashree <59689342+Divyashree-123@users.noreply.github.com>
2026-06-09 19:23:55 +0530

    var let and const assignments

---
+++ varLetConstAssignment.js
@@ -0,0 +1,15 @@
+const browserVersion = "Chrome"
+function getBrowserVersion(){
+    if(true){
+        var browserVersion = "Safari"
+        console.log(browserVersion);
+    }
+}
+function getBrowserVersion1(){
+    if(true){
+        let browserVersion = "Opera"
+        console.log(browserVersion);
+    }
+}
+getBrowserVersion()
+getBrowserVersion1()
