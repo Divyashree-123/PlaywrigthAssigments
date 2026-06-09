6e39e50d85c6d75873b6edd6f6f9d3bdccc07493
Divyashree <59689342+Divyashree-123@users.noreply.github.com>
2026-06-09 19:27:30 +0530

    Correct variable declarations in browser version functions
    Fix variable shadowing issue in getBrowserVersion and getBrowserVersion1 functions.

--- varLetConstAssignment.js
+++ varLetConstAssignment.js
@@ -1,15 +1,15 @@
 const browserVersion = "Chrome"
 function getBrowserVersion(){
     if(true){
-        var browserVersion = "Safari"
-        console.log(browserVersion);
+        var browserVersion = "Safari"    
     }
+    console.log(browserVersion);
 }
 function getBrowserVersion1(){
     if(true){
         let browserVersion = "Opera"
-        console.log(browserVersion);
     }
+     console.log(browserVersion);
 }
 getBrowserVersion()
 getBrowserVersion1()
