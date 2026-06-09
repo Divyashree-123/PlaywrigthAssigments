7bd60959503e8a53a79deb36aca1c063ccbd0e94
Divyashree <59689342+Divyashree-123@users.noreply.github.com>
2026-06-09 16:56:31 +0530

    Day1 Assigments added

---
+++ dataTypeAssignment.js
@@ -0,0 +1,7 @@
+let firstName = "Divyashree"
+let lastName = "SS"
+let mobileNumber = 1234567890
+let isAutomation = true
+let playright
+console.log(firstName, lastName, mobileNumber, isAutomation, playright)
+console.log(typeof firstName, typeof lastName, typeof mobileNumber, typeof isAutomation, typeof playright)
